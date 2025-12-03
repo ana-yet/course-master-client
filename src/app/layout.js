fc51949be2e2e8181334b1d54fb0a7eb6367ffe0
ana-yet <anayethusenaj@gmail.com>
2025-12-04 01:23:03 +0600

    react hot toast initilized

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -15,8 +16,29 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* Apply the variable to the body */}
       <body className={`${inter.variable} antialiased`}>{children}</body>
+
+      {/* react hot toast */}
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: "#333",
+            color: "#fff",
+          },
+          success: {
+            style: {
+              background: "#10b981", // Emerald-500
+            },
+          },
+          error: {
+            style: {
+              background: "#e11d48", // Rose-600
+            },
+          },
+        }}
+      />
     </html>
   );
 }
