4e53a63e61dd621a649c561ff4d9fb0d781f6f74
ana-yet <anayethusenaj@gmail.com>
2025-12-03 18:36:20 +0600

    initilized project

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,10 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * Merges tailwind classes intelligently.
+ * Example: cn("bg-red-500", "bg-blue-500") -> "bg-blue-500" (Last one wins)
+ */
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
