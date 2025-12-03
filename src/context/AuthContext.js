f915dd5c0e369a228f4db11f5647d94f5c7fded3
ana-yet <anayethusenaj@gmail.com>
2025-12-04 01:41:22 +0600

    created auth context

---
+++ src/context/AuthContext.js
@@ -0,0 +1,66 @@
+"use client";
+
+import { createContext, useContext, useState, useEffect } from "react";
+import { authService } from "@/services/auth.service";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true); // while auth is loading
+  const router = useRouter();
+
+  // 1. Check if user is logged in on initial load
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await authService.getCurrentUser();
+        // If success, save user data to state
+        setUser(response.data);
+      } catch (error) {
+        // If error (401), user is guest
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  // 2. Login Function
+  const login = async (credentials) => {
+    const response = await authService.login(credentials);
+    setUser(response.data.user); // Update state immediately
+    return response;
+  };
+
+  // 3. Register Function
+  const register = async (userData) => {
+    return await authService.register(userData);
+  };
+
+  // 4. Logout Function
+  const logout = async () => {
+    try {
+      await authService.logout();
+      setUser(null);
+      toast.success("Logged out successfully");
+      router.push("/login");
+    } catch (error) {
+      toast.error("Logout failed");
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, loading, login, register, logout }}>
+      {!loading && children}
+      {/* TODO: Add  loading spinner here if loading is true */}
+    </AuthContext.Provider>
+  );
+}
+
+// Custom Hook to use the context easily
+export const useAuth = () => useContext(AuthContext);
